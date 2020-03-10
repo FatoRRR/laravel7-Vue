9e784cd6fa7172407b46317478cc56b3bcd121d4
FatmirRragomaj <fatrrag@gmail.com>
2020-03-10 12:10:37 +0100

    Initialize vue, vue-router and vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-require('./bootstrap');
+window._ = require('lodash');
+window.axios = require('axios');
+
+import Vue from 'vue'
+import router from './router/router'
+import store from './vuex/store'
+
+import App from 'PAGE/App'
+
+const app = new Vue({
+  el: '#app',
+  components: {
+    App,
+  },
+  router,
+  store,
+});
