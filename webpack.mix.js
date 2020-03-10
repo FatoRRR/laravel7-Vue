5779a8e34b0f77a051b2be8453c25415d58e6720
FatmirRragomaj <fatrrag@gmail.com>
2020-03-10 12:07:52 +0100

    Add aliases to vue components

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,19 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+mix.webpackConfig({
+    resolve: {
+        alias: {
+            'PAGE': path.resolve(__dirname, 'resources/js/components/pages'),
+            'ATOM': path.resolve(__dirname, 'resources/js/components/atoms'),
+            'MOLECULE': path.resolve(__dirname, 'resources/js/components/molecules'),
+            'ORGANISM': path.resolve(__dirname, 'resources/js/components/organisms'),
+            'TEMPLATE': path.resolve(__dirname, 'resources/js/components/templates'),
+        }
+    },
+    module: {
+        rules: []
+    }
+});
